c4602f9374e2c54fcce203f467e853df21ad4e39
Manuel254 <Immanuelkituku@gmail.com>
2023-06-14 13:34:36 +0300

    Remove redundant book_adv view

--- booking/urls.py
+++ booking/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('adventures/', views.adventures, name='adventures'),
     path('adventures/<str:id>', views.adv_details, name='adventure'),
-    path('<str:id>/booking', views.book_adv, name='booking'),
     path('<str:id>/order', views.order_page, name='order'),
-    # path('order/', views.order_page),
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_page, name='logout'),  
     path('signup/', views.signup_page, name='signup')
